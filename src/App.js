4d98931e2491b3c3be63ac4fef1fc699889cc3c8
Seong-Myeong <mg0454@naver.com>
2024-12-03 06:30:50 +0900

    될 것 같음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom"; // 라우터 import
+import { Routes, Route } from "react-router-dom"; // 라우터 import
 import LandingPage from "./pages/LandingPage";
 import SignPage from "./pages/SignPage";
 //import OnBoardingPage from "./pages/OnBoardingPage";
@@ -28,46 +28,44 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signpage" element={<SignPage />} />
-        <Route path="/termspage" element={<TermsPage />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signpage" element={<SignPage />} />
+      <Route path="/termspage" element={<TermsPage />} />
+      <Route path="/privacypolicy" element={<PrivacyPolicy />} />
 
-        <Route
-          path="/homepage"
-          element={
-            <HomePage
-              bookmarks={bookmarks}
-              onAddBookmark={handleAddBookmark}
-              onDeleteBookmark={handleDeleteBookmark}
-            />
-          }
-        />
+      <Route
+        path="/homepage"
+        element={
+          <HomePage
+            bookmarks={bookmarks}
+            onAddBookmark={handleAddBookmark}
+            onDeleteBookmark={handleDeleteBookmark}
+          />
+        }
+      />
 
-        <Route
-          path="/folderpage"
-          element={
-            <FolderPage
-              bookmarks={bookmarks}
-              onAddBookmark={handleAddBookmark}
-              onDeleteBookmark={handleDeleteBookmark}
-            />
-          }
-        />
-        <Route
-          path="/removeditemspage"
-          element={
-            <RemovedItemsPage
-              bookmarks={bookmarks}
-              onAddBookmark={handleAddBookmark}
-              onDeleteBookmark={handleDeleteBookmark}
-            />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+      <Route
+        path="/folderpage"
+        element={
+          <FolderPage
+            bookmarks={bookmarks}
+            onAddBookmark={handleAddBookmark}
+            onDeleteBookmark={handleDeleteBookmark}
+          />
+        }
+      />
+      <Route
+        path="/removeditemspage"
+        element={
+          <RemovedItemsPage
+            bookmarks={bookmarks}
+            onAddBookmark={handleAddBookmark}
+            onDeleteBookmark={handleDeleteBookmark}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
