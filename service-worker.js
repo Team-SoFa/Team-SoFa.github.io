8f5932cde7bcda5f9461096ac08685fcf7183ba0
Seong-Myeong <Seong-Myeong@users.noreply.github.com>
2024-12-02 22:37:30 +0000

    deploy: 91951de4d58bd739ae993c4ecbbb8e4311fd518c

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ac6f207a8238a257dde9584179b5d0c.js"
+  "/Team-SoFa.github.io/precache-manifest.bfe897f17f0aba7c900a2ac750126fe3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Team-SoFa.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
