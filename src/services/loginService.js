094404038ffebbf5bebf0dba23046901cb24b436
Seong-Myeong <mg0454@naver.com>
2024-12-03 03:39:54 +0900

    CI/CD TEST 5

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -12,13 +12,13 @@ export const getGoogleLoginUrl = async () => {
   }
 };
 
-// export const acessToken = async () => {
-//   try {
-//     console.log("Base URL:", apiClient.defaults.baseURL);
-//     const response = await apiClient.get("/login/oauth2/code/google");
-//     return response.data;
-//   } catch (error) {
-//     console.error("Failed to fetch Google login URL:", error);
-//     throw error;
-//   }
-// };
+export const acessToken = async () => {
+  try {
+    console.log("Base URL:", apiClient.defaults.baseURL);
+    const response = await apiClient.get("/login/oauth2/code/google");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch Google login URL:", error);
+    throw error;
+  }
+};
