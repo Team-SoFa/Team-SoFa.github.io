edaba896abf1906ef5cf8e07031700e0304a0556
Seong-Myeong <mg0454@naver.com>
2024-12-03 07:46:01 +0900

    환경 재설정..

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/Typography/Typography.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* basename 설정 */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
