ed52fbb2022255170f00368012e996ad399c96fb
Seong-Myeong <mg0454@naver.com>
2024-12-05 07:00:59 +0900

    TEST

---
+++ precache-manifest.4c9de86648f4fe7b89409c5e37629e2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f095ef4e25faccd632e80676ccd5aac4",
+    "url": "/Team-SoFa.github.io/index.html"
+  },
+  {
+    "revision": "c031488ac432cd7d8f78",
+    "url": "/Team-SoFa.github.io/static/css/main.30fa7c1c.chunk.css"
+  },
+  {
+    "revision": "00bd645ce4d5c3a44866",
+    "url": "/Team-SoFa.github.io/static/js/2.75631837.chunk.js"
+  },
+  {
+    "revision": "0d3236f0da39143fd5a0e10aed39db9e",
+    "url": "/Team-SoFa.github.io/static/js/2.75631837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c031488ac432cd7d8f78",
+    "url": "/Team-SoFa.github.io/static/js/main.748d3781.chunk.js"
+  },
+  {
+    "revision": "5b7044b2047c10c3480c",
+    "url": "/Team-SoFa.github.io/static/js/runtime-main.f7c54f0c.js"
+  },
+  {
+    "revision": "40b92fb6e0e447ca7f0c7fddb6cec9c6",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Bold.subset.40b92fb6.woff2"
+  },
+  {
+    "revision": "6dec4607c4804775486c0b22e1b96f4b",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-ExtraBold.subset.6dec4607.woff2"
+  },
+  {
+    "revision": "e55a007a88db0a7a45bbeedd0bc9ce77",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Medium.subset.e55a007a.woff2"
+  },
+  {
+    "revision": "97213db5c578498e3bab52fb858e720a",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Regular.subset.97213db5.woff2"
+  },
+  {
+    "revision": "3ba9cd1e8bcb4824f073865d2ed81d34",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-SemiBold.subset.3ba9cd1e.woff2"
+  }
+]);
