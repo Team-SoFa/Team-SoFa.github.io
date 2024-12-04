85a059e501210fe757adb8e44c5e2c57e5dc67e7
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2024-12-05 06:23:50 +0900

    Add files via upload

---
+++ precache-manifest.465f2582f1f83a8eeefd8c546d237bbe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3339b34129c51c5a4b19d28c17d7a8ac",
+    "url": "/Team-SoFa.github.io/index.html"
+  },
+  {
+    "revision": "1b1ab7fa3761f7379b76",
+    "url": "/Team-SoFa.github.io/static/css/main.30fa7c1c.chunk.css"
+  },
+  {
+    "revision": "00bd645ce4d5c3a44866",
+    "url": "/Team-SoFa.github.io/static/js/2.75631837.chunk.js"
+  },
+  {
+    "revision": "0d3236f0da39143fd5a0e10aed39db9e",
+    "url": "/Team-SoFa.github.io/static/js/2.75631837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1ab7fa3761f7379b76",
+    "url": "/Team-SoFa.github.io/static/js/main.a82c83ca.chunk.js"
+  },
+  {
+    "revision": "5b7044b2047c10c3480c",
+    "url": "/Team-SoFa.github.io/static/js/runtime-main.f7c54f0c.js"
+  },
+  {
+    "revision": "40b92fb6e0e447ca7f0c7fddb6cec9c6",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Bold.subset.40b92fb6.woff2"
+  },
+  {
+    "revision": "6dec4607c4804775486c0b22e1b96f4b",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-ExtraBold.subset.6dec4607.woff2"
+  },
+  {
+    "revision": "e55a007a88db0a7a45bbeedd0bc9ce77",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Medium.subset.e55a007a.woff2"
+  },
+  {
+    "revision": "97213db5c578498e3bab52fb858e720a",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-Regular.subset.97213db5.woff2"
+  },
+  {
+    "revision": "3ba9cd1e8bcb4824f073865d2ed81d34",
+    "url": "/Team-SoFa.github.io/static/media/Pretendard-SemiBold.subset.3ba9cd1e.woff2"
+  }
+]);
